60c908915ff890ef27f7fe880ecc6efff9b6ca59
Rebekah Felker <rebekahfelker@gmail.com>
2022-06-30 00:57:24 -0400

    responsive styling

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './styles.module.css';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+
     return (
         <header className={styles.appHeader}>
         <div className={styles.contentContainer}>
-          <Link to="/">about</Link>
-          <Link to="/about">past projects</Link>
+          <NavLink 
+            to="/"
+            className={({ isActive }) => isActive ? `${styles.linkStyle}` : undefined }
+          >about</NavLink>
+          <NavLink 
+            to="/about"
+            className={({ isActive }) => isActive ? `${styles.linkStyle}` : undefined }
+          >past projects</NavLink>
           <a target="_blank" href="https://github.com/rbkh/bek-portfolio">github</a>
           <a target="_blank" href="https://www.linkedin.com/in/rebekahfelker/">linkedin</a>
           </div>   
